269acbe7499a9690f70af5eed2b6cdd8776c3711
Paschalis T <paschalist0@gmail.com>
2019-01-23 18:34:24 +0200

    CORRECTED : Step 3: add more spheres

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ type Sphere struct {
 	Material Materials
 }
 
-func (s Sphere) ray_intersect(orig, dir r3.Vector, t0 float64) bool {
+func (s Sphere) ray_intersect(orig, dir r3.Vector, t0 *float64) bool {
 	L := r3.Vector.Sub(s.Center, orig)
 	tca := r3.Vector.Dot(L, dir)
 	d2 := r3.Vector.Dot(L, L) - tca*tca
@@ -34,26 +34,26 @@ func (s Sphere) ray_intersect(orig, dir r3.Vector, t0 float64) bool {
 	}
 
 	thc := math.Sqrt(s.Radius*s.Radius - d2)
-	t0 = tca - thc
+	*t0 = tca - thc
 	t1 := tca + thc
 
-	if t0 < 0 {
-		t0 = t1
+	if *t0 < 0.0 {
+		*t0 = t1
 	}
-	if t0 < 0 {
+	if *t0 < 0.0 {
 		return false
 	}
 	return true
 }
 
-func scene_intersect(orig, dir r3.Vector, spheres []Sphere, hit, N r3.Vector, material *Materials) bool {
+func scene_intersect(orig, dir r3.Vector, spheres []Sphere, hit, N *r3.Vector, material *Materials) bool {
 	spheres_dist := math.MaxFloat64
 	for i := 0; i < len(spheres); i++ {
-		var dist_i float64
-		if spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < spheres_dist {
+        dist_i := 0.0
+		if spheres[i].ray_intersect(orig, dir, &dist_i) && dist_i < spheres_dist {
 			spheres_dist = dist_i
-			hit = r3.Vector.Add(orig, r3.Vector.Mul(dir, dist_i))
-			N = r3.Vector.Normalize(r3.Vector.Sub(hit, spheres[i].Center))
+			*hit = r3.Vector.Add(orig, r3.Vector.Mul(dir, dist_i))
+			*N = r3.Vector.Normalize(r3.Vector.Sub(*hit, spheres[i].Center))
 			*material = spheres[i].Material // Declared and not used for commit 3
 			_ = material
 		}
@@ -65,7 +65,7 @@ func cast_ray(orig, dir r3.Vector, spheres []Sphere) color.RGBA {
 	var point, N r3.Vector
 	var material Materials
 
-	if !scene_intersect(orig, dir, spheres, point, N, &material) {
+	if !scene_intersect(orig, dir, spheres, &point, &N, &material) {
 		return color.RGBA{50, 180, 205, 255}
 	}
 	return material.DiffusedColor
@@ -84,8 +84,7 @@ func render(spheres []Sphere) {
 			var x, y float64
 			x = (2.0*(float64(i)+0.5)/w - 1.0) * math.Tan(fov/2.0) * w / h
 			y = -(2.0*(float64(j)+0.5)/h - 1.0) * math.Tan(fov/2.0)
-			dir := r3.Vector{x, y, -1.0}
-			dir = r3.Vector.Normalize(dir)
+			dir := r3.Vector.Normalize(r3.Vector{x, y, -1.0})
 			img.Set(i, j, cast_ray(r3.Vector{0, 0, 0}, dir, spheres))
 		}
 	}
@@ -107,6 +106,8 @@ func main() {
 	spheres = append(spheres, Sphere{r3.Vector{-1.0, -1.5, -12.0}, 2, red_rubber})
 	spheres = append(spheres, Sphere{r3.Vector{1.5, -0.5, -18.0}, 3, red_rubber})
 	spheres = append(spheres, Sphere{r3.Vector{7.0, 5.0, -18.0}, 4, ivory})
+
+
 
 	render(spheres)
 }
