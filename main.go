ef5b71af172fc85cadac00e574def28984c6462a
Paschalis T <paschalist0@gmail.com>
2019-01-24 17:16:11 +0200

    Step 8: refractions

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ type Light struct {
 }
 
 type Materials struct {
+    RefractiveIndex float64
     Albedo []float64
 	DiffusedColor color.RGBA
     SpecularExponent float64
@@ -78,15 +79,28 @@ func cast_ray(orig, dir r3.Vector, spheres []Sphere, lights []Light, depth int) 
 		return color.RGBA{50, 180, 205, 255}
 	}
 
-    reflect_dir := r3.Vector.Normalize(Reflect(dir, N))
     var reflect_orig r3.Vector
+    var refract_orig r3.Vector
+
+    reflect_dir := r3.Vector.Normalize(Reflect(dir, N))
+    refract_dir := r3.Vector.Normalize(Refract(dir, N, material.RefractiveIndex))
 
     if r3.Vector.Dot(reflect_dir, N) < 0 {
         reflect_orig = r3.Vector.Sub(point, r3.Vector.Mul(N, 0.001))
     } else {
         reflect_orig = r3.Vector.Add(point, r3.Vector.Mul(N, 0.001))
     }
+
+    if r3.Vector.Dot(refract_dir, N) < 0 {
+        refract_orig = r3.Vector.Sub(point, r3.Vector.Mul(N, 0.001))
+    } else {
+        refract_orig = r3.Vector.Add(point, r3.Vector.Mul(N, 0.001))
+    }
+
     reflect_color := cast_ray(reflect_orig, reflect_dir, spheres, lights, depth)
+    refract_color := cast_ray(refract_orig, refract_dir, spheres, lights, depth)
+
+
 
 	//return material.DiffusedColor
     diffuse_light_intensity, specular_light_intensity := 0.0, 0.0
@@ -124,7 +138,12 @@ func cast_ray(orig, dir r3.Vector, spheres []Sphere, lights []Light, depth int) 
     res3x := float64(reflect_color.R) * material.Albedo[2]
     res3y := float64(reflect_color.G) * material.Albedo[2]
     res3z := float64(reflect_color.B) * material.Albedo[2]
-    return AddColors(r3.Vector{res1x, res1y, res1z}, r3.Vector{res2x, res2y, res2z}, r3.Vector{res3x, res3y, res3z})
+
+    res4x := float64(refract_color.R) * material.Albedo[3]
+    res4y := float64(refract_color.G) * material.Albedo[3]
+    res4z := float64(refract_color.B) * material.Albedo[3]
+
+    return AddColors(r3.Vector{res1x, res1y, res1z}, r3.Vector{res2x, res2y, res2z}, r3.Vector{res3x, res3y, res3z}, r3.Vector{res4x, res4y, res4z})
 }
 
 func render(spheres []Sphere, lights []Light) {
@@ -164,8 +183,28 @@ func Reflect (I, N r3.Vector) r3.Vector {
     return r3.Vector.Sub(I, r3.Vector.Mul(N,2.0*r3.Vector.Dot(I, N)))
 }
 
-func AddColors(i, j, k r3.Vector) color.RGBA {
-    r, g, b := (i.X + j.X + k.X), (i.Y + j.Y + k.Y), (i.Z + j.Z + k.Z)
+func Refract(I, N r3.Vector, refractiveIdx float64) r3.Vector {
+    // Snell's law
+    cosi := - max(-1., min(1, r3.Vector.Dot(I, N)))
+    etai, etat := 1., refractiveIdx
+    n := N
+    if cosi < 0. {
+        // if the ray is inside the object, swap the indices and invert the normal to get the correct result
+        cosi = -cosi
+        etai, etat = swap(etai, etat)
+        n = r3.Vector.Mul(N, -1)
+    }
+    eta := etai/etat
+    k := 1. - eta*eta*(1.-cosi*cosi)
+    if k < 0. {
+        return r3.Vector{0., 0., 0.}
+    } else {
+        return r3.Vector.Add( r3.Vector.Mul(I, eta), r3.Vector.Mul(n, (eta * cosi - math.Sqrt(k))))
+    }
+}
+
+func AddColors(i, j, k, l r3.Vector) color.RGBA {
+    r, g, b := (i.X + j.X + k.X + l.X), (i.Y + j.Y + k.Y + l.Y), (i.Z + j.Z + k.Z + l.Z)
     maxc := float64(max(float64(r), max(float64(g), float64(b))))
     if maxc > 255. {
         return color.RGBA{uint8(float64(r)*255./maxc),
@@ -181,17 +220,16 @@ func AddColors(i, j, k r3.Vector) color.RGBA {
 
 func main() {
 
-	ivory       := Materials{Albedo: []float64{0.3, 0.6, 0.1}, DiffusedColor: color.RGBA{100, 100, 75, 255}, SpecularExponent: 50.}
-	red_rubber  := Materials{Albedo: []float64{0.9, 0.1, 0.0}, DiffusedColor: color.RGBA{75, 25, 25, 255}, SpecularExponent: 10.}
-    mirror      := Materials{Albedo: []float64{0.0, 10.0, 0.8}, DiffusedColor: color.RGBA{255, 255, 255, 255}, SpecularExponent: 1425.}
+	ivory       := Materials{RefractiveIndex: 1.0, Albedo: []float64{0.3, 0.6, 0.1, 0.0}, DiffusedColor: color.RGBA{100, 100, 75, 255}, SpecularExponent: 50.}
+    glass       := Materials{RefractiveIndex: 1.5, Albedo: []float64{0.0, 0.5, 0.1, 0.8}, DiffusedColor: color.RGBA{255, 255, 255, 255}, SpecularExponent: 1425.}
+	red_rubber  := Materials{RefractiveIndex: 1.0, Albedo: []float64{0.9, 0.1, 0.0, 0.0}, DiffusedColor: color.RGBA{76, 25, 25, 255}, SpecularExponent: 10.}
+    mirror      := Materials{RefractiveIndex: 1.0, Albedo: []float64{0.0, 10.0, 0.8, 0.0}, DiffusedColor: color.RGBA{255, 255, 255, 255}, SpecularExponent: 1425.}
 
 	spheres := make([]Sphere, 0)
 	spheres = append(spheres, Sphere{r3.Vector{-3.0, 0.0, -16.0}, 2, ivory})
-	spheres = append(spheres, Sphere{r3.Vector{-1.0, -1.5, -12.0}, 2, mirror})
+	spheres = append(spheres, Sphere{r3.Vector{-1.0, -1.5, -12.0}, 2, glass})
 	spheres = append(spheres, Sphere{r3.Vector{1.5, -0.5, -18.0}, 3, red_rubber})
 	spheres = append(spheres, Sphere{r3.Vector{7.0, 5.0, -18.0}, 4, mirror})
-	//spheres = append(spheres, Sphere{r3.Vector{-1.0, -1.5, -12.0}, 2, red_rubber})
-	//spheres = append(spheres, Sphere{r3.Vector{7.0, 5.0, -18.0}, 4, ivory})
 
     lights := make([]Light, 0)
     lights = append(lights, Light{r3.Vector{-20, 20, 20}, 1.5})
@@ -208,3 +246,12 @@ func multiplyColorIntensity(c color.RGBA, f float64) color.RGBA {
                       255}
 }
 
+func swap(a, b float64) (float64, float64) {
+    return b, a
+}
+func min(a, b float64) float64 {
+    if a < b {
+        return a
+    }
+    return b
+}
